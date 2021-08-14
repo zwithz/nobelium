306ce7acc1c48599a140a95d5c1ae48200b7b486
Zack <zwithz1998@gmail.com>
2021-08-15 00:19:04 +0800

    Update

--- blog.config.js
+++ blog.config.js
@@ -1,61 +1,61 @@
 const BLOG = {
-  title: 'Zack\'s Blog',
-  author: 'Zack Zhou',
-  email: ' zwithz1998@gmail.com',
-  link: 'https://nobelium.vercel.app',
-  description: 'Coder👨🏻‍💻 && Plogger🗣 && 摄影爱好者📸',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Nobelium in a folder
+  title: "Zack's Blog",
+  author: "Zack Zhou",
+  email: " zwithz1998@gmail.com",
+  link: "https://blog.zackzhou.com",
+  description: "",
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  appearance: "auto", // ['light', 'dark', 'auto'],
+  font: "serif", // ['sans-serif', 'serif']
+  lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: "#18181B", // use hex value, don't forget '#'
+  path: "", // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/zwithz1998',
+  ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
+  socialLink: "https://twitter.com/zwithz1998",
   seo: {
-    keywords: ['Blog', 'Website', 'Computer Vision', 'iOS', 'Swift', 'SwiftUI'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    keywords: ["Blog", "Website", "Computer Vision", "iOS", "Swift", "SwiftUI"],
+    googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: "ga", // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: "", // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: "", // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
-    }
+      measurementId: "G-D4N1VCFL35", // e.g: G-XXXXXXXXXX
+    },
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: "", // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: '', // The repository of store comments
-      owner: '',
+      repo: "", // The repository of store comments
+      owner: "",
       admin: [],
-      clientID: '',
-      clientSecret: '',
-      distractionFreeMode: false
+      clientID: "",
+      clientSecret: "",
+      distractionFreeMode: false,
     },
     utterancesConfig: {
-      repo: ''
+      repo: "",
     },
     cusdisConfig: {
-      appId: '', // data-app-id
-      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
-      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
-    }
+      appId: "", // data-app-id
+      host: "https://cusdis.com", // data-host, change this if you're using self-hosted version
+      scriptSrc: "https://cusdis.com/js/cusdis.es.js", // change this if you're using self-hosted version
+    },
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-}
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+};
 // export default BLOG
 module.exports = BLOG
